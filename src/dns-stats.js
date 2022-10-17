c907bf7dc17f9bb6a92d5ee052055834c73503b3
Olga Kharkevich <olga.kharkevich@gmail.com>
2022-10-17 18:55:01 -0400

    feat: add solution for dns-stats

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  const dnsObj = {};
+  domains.forEach((str) => {
+    let prop = '';
+    const arr = str.split('.').reverse();
+    arr.forEach((el) => {
+      prop += `.${el}`;
+      dnsObj[prop] = (dnsObj[prop]) ? dnsObj[prop] + 1 : 1;
+    });
+  });
+
+  return dnsObj;
 }
 
 module.exports = {
